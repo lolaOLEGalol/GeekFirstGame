9f6d7e1109cd89d4f98f7203e841d041c2df1e1b
oleg <experementator@gmail.com>
2020-12-24 13:14:48 +0300

    Animation

--- GeekBrains1LABIRINTE/Assets/Script/NavigationEnemy.cs
+++ GeekBrains1LABIRINTE/Assets/Script/NavigationEnemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class NavigationEnemy : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class NavigationEnemy : MonoBehaviour
     private float timePatrul = 2f;
     private float waitPatrul;
     private float waitAtack = 0;
+
+    
 
     void Start()
     {
@@ -22,15 +25,27 @@ public class NavigationEnemy : MonoBehaviour
     {
         if (other.tag == "checkEnemy")
         {
+            GetComponent<Animator>().SetBool("is_walk", false);
             Debug.Log("УВИДЕЛ ИГРОКА!");
+
+            GetComponent<Animator>().SetBool("is_run", true);
+            GetComponent<NavMeshAgent>().speed = 5;
             GetComponent<NavMeshAgent>().SetDestination(playerPoint.position);
 
             if (Vector3.Distance(transform.position, playerPoint.position) < 1f && waitAtack <= 0)
             {
+                SceneManager.LoadScene(0);
                 Debug.Log("УБИЛ!");
                 waitAtack = timePatrul;
             }
-            waitAtack -= Time.deltaTime;
+            if (Vector3.Distance(transform.position, playerPoint.position) < 3f)
+            {
+                GetComponent<Animator>().SetBool("is_atack", true);
+            }
+            if (Vector3.Distance(transform.position, playerPoint.position) > 3f)
+            {
+                GetComponent<Animator>().SetBool("is_atack", false);
+            }
         }
     }
 
@@ -38,16 +53,23 @@ public class NavigationEnemy : MonoBehaviour
     {
         if (other.tag == "checkEnemy")
         {
-           GetComponent<NavMeshAgent>().SetDestination(startPoint.position);
+            GetComponent<Animator>().SetBool("is_atack", false);
+            GetComponent<Animator>().SetBool("is_run", false);
+            GetComponent<Animator>().SetBool("is_walk", true);
+            GetComponent<NavMeshAgent>().speed = 2;
+            GetComponent<NavMeshAgent>().SetDestination(startPoint.position);
         }
     }
 
+    
     private void Check()
     {
-        if (Vector3.Distance(transform.position, finishPoint.position) < 1)
+        if (Vector3.Distance(transform.position, finishPoint.position) < 0.1f)
         {
-            if(waitPatrul <= 0)
+            GetComponent<Animator>().SetBool("is_idle", true);
+            if (waitPatrul <= 0)
             {
+                GetComponent<Animator>().SetBool("is_walk", true);
                 GetComponent<NavMeshAgent>().SetDestination(startPoint.position);
                 Debug.Log("Достиг финиша");
                 waitPatrul = timePatrul;
@@ -56,15 +78,21 @@ public class NavigationEnemy : MonoBehaviour
             waitPatrul -= Time.deltaTime;
             
         }
-        else if (Vector3.Distance(transform.position, startPoint.position) < 1)
+        else if (Vector3.Distance(transform.position, startPoint.position) < 0.1f)
         {
+            GetComponent<Animator>().SetBool("is_idle", true);
             if (waitPatrul <= 0)
             {
+                GetComponent<Animator>().SetBool("is_walk", true);
                 GetComponent<NavMeshAgent>().SetDestination(finishPoint.position);
                 Debug.Log("Достиг старта");
                 waitPatrul = timePatrul;
             }
             waitPatrul -= Time.deltaTime;
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("is_idle", false);
         }
 
         
--- GeekBrains1LABIRINTE/Assets/Script/Player2.cs
+++ GeekBrains1LABIRINTE/Assets/Script/Player2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player2 : MonoBehaviour
 {
-    [SerializeField] private float speed = 3;
+    [SerializeField] private float speed = 2.5f;
     [SerializeField] private float speedRotation = 3;
     [SerializeField] private float jump;
     private float verticalMove;
@@ -15,6 +15,7 @@ public class Player2 : MonoBehaviour
     private void Awake()
     {
         key = 0;
+        UnityEngine.Cursor.visible = false;
     }
 
 
@@ -42,5 +43,53 @@ public class Player2 : MonoBehaviour
     {
         Transform();
         Rotate();
+        Animation();
+    }
+
+    
+    void Animation()
+    {
+
+        //Вперед
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            GetComponent<Animator>().SetBool("is_walk", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("is_walk", false);
+        }
+        //Назад
+        if(Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Animator>().SetBool("is_walkBack", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("is_walkBack", false);
+        }
+        // Бег
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            GetComponent<Animator>().SetBool("is_run", true);
+            speed = 7;
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("is_run", false);
+            speed = 2.5f;
+        }
+        // Стороны
+        if(Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") == 0)
+        {
+            GetComponent<Animator>().SetBool("is_walk", true);
+        }
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            GetComponent<Animator>().SetBool("is_walk", false);
+        }
+
+
+
     }
 }
